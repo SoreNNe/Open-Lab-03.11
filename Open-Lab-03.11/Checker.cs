8021a03ae303452d11e6a97a896c406c6a1ae1aa
SoreNNe <71930836+SoreNNe@users.noreply.github.com>
2020-11-07 02:38:21 +0100

    .

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_03._11
 {
@@ -6,15 +7,7 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            string first = str.Substring(0, str.Length / 2);
-            char[] arr = str.ToCharArray();
-
-            Array.Reverse(arr);
-
-            string temp = new string(arr);
-            string second = temp.Substring(0, temp.Length / 2);
-
-            return first.Equals(second);
+            return str == new string(str.Reverse().ToArray());
         } 
     }
 }
